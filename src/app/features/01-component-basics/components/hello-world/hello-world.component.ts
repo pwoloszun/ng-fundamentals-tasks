800faa4151d65a7405c037fb07e1eb97976e0068
pwoloszun <piotr.woloszun@gmail.com>
2021-07-26 14:22:14 +0200

    ngfor

--- src/app/features/01-component-basics/components/hello-world/hello-world.component.ts
+++ src/app/features/01-component-basics/components/hello-world/hello-world.component.ts
@@ -12,6 +12,17 @@ export class HelloWorldComponent implements OnInit {
     age: 123
   };
 
+  people = [
+    { id: 100, name: 'bob' },
+    { id: 200, name: 'ed' },
+    { id: 300, name: 'kate' },
+
+  ];
+
+  personClick(per: any) {
+    console.log('person', per);
+  }
+
   ngOnInit() {
     // this.name = 'qq';
   }
