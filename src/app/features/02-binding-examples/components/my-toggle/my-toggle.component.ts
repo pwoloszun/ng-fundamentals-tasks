c42733324679b6192a8281916ba9ba72a24fe9e0
pwoloszun <piotr.woloszun@gmail.com>
2021-07-26 12:03:03 +0200

    toggle p2

--- src/app/features/02-binding-examples/components/my-toggle/my-toggle.component.ts
+++ src/app/features/02-binding-examples/components/my-toggle/my-toggle.component.ts
@@ -7,6 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyToggleComponent implements OnInit {
 
+  // primary (aka essential) state
+  isVisible = true;
+
+  // computed (aka derived) state
+  // cssClasses = {
+  //   'mat-primary': true,
+  //   'mat-warn': false
+  // };
+  // buttonText = 'Toggle Hide';
+
+  get cssClasses() {
+    if (this.isVisible) {
+      return {
+        'mat-primary': true,
+        'mat-warn': false
+      };
+    } else {
+      return {
+        'mat-primary': false,
+        'mat-warn': true
+      };
+    }
+  }
+
+  get buttonText() {
+    if (this.isVisible) {
+      return `Toggle Hide`;
+    } else {
+      return `Toggle Show`;
+    }
+  }
+
+  toggleHandler() {
+    this.isVisible = !this.isVisible;
+    // if (this.isVisible) {
+    //   this.cssClasses = {
+    //     'mat-primary': true,
+    //     'mat-warn': false
+    //   };
+    //   this.buttonText = `Toggle Hide`;
+    // } else {
+    //   this.cssClasses = {
+    //     'mat-primary': false,
+    //     'mat-warn': true
+    //   };
+    //   this.buttonText = `Toggle Show`;
+    // }
+  }
+
   ngOnInit() {
   }
 
