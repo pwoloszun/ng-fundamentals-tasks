1ffbc39374b3b8104cb4b61a33994153e1dc959a
pwoloszun <piotr.woloszun@gmail.com>
2021-07-27 17:00:16 +0200

    quick search

--- src/app/features/39-rxjs-gui-tasks/containers/my-search/my-search.component.ts
+++ src/app/features/39-rxjs-gui-tasks/containers/my-search/my-search.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { of, Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap, mergeMap, exhaustMap, delay } from 'rxjs/operators';
+import {
+  debounceTime, distinctUntilChanged, switchMap, mergeMap, exhaustMap, delay,
+  mergeAll, switchAll, filter, map
+} from 'rxjs/operators';
 
 import { SearchApiService } from '@api/search-api.service';
 
@@ -10,32 +13,19 @@ import { SearchApiService } from '@api/search-api.service';
   templateUrl: './my-search.component.html',
   styleUrls: ['./my-search.component.css']
 })
-export class MySearchComponent implements OnDestroy {
+export class MySearchComponent {
 
   searchTextCtrl = new FormControl('');
 
-  // TODO
-  searchResults$ = of([
-    'bob',
-    'batman',
-    'imba!'
-  ]).pipe(
-    delay(1500)
+  searchResults$ = this.searchTextCtrl.valueChanges.pipe(
+    debounceTime(600),
+    filter((query) => query.length >= 2),
+    distinctUntilChanged(),
+    map((query) => this.searchApiService.querySearch$(query)),
+    switchAll()
   );
 
-  // results: string[];
-
-  // private subscriptions: Subscription[] = [];
-
   constructor(private searchApiService: SearchApiService) {
-    // const sub = this.searchResults$.subscribe(
-    //   (val) => this.results = val
-    // );
-    // this.subscriptions.push(sub);
-  }
-
-  ngOnDestroy() {
-    //   this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
 }
